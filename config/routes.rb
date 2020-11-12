9b7339c8537460d42c60ca3177f73bb8f56eca3c
marfuto <marfuto1@gmail.com>
2020-11-12 14:37:58 +0200

    blank home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'home_page/home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home_page#home'
 end
