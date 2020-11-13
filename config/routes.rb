9a5e065c54b303bce3ce97ff9be0332f9c3dda52
marfuto <marfuto1@gmail.com>
2020-11-13 15:49:31 +0200

    unworking modal windows

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'session/new'
   root 'home_page#home'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
 end
